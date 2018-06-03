1fc5eed23fbe738eac09cd9702e4c23452cdbed3
Eric Perez <ibestudios@gmail.com>
2018-06-03 12:52:58 -0700

    routing through node. added login page.

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-var http = require('http');
+var url = require('url');
 var fs = require('fs');
 
-function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./index.html', null, function(error, data) {
+function renderHTML(path, response) {
+    fs.readFile(path, null, function(error, data) {
         if (error) {
             response.writeHead(404);
             response.write('File not found!');
@@ -14,5 +13,23 @@ function onRequest(request, response) {
     });
 }
 
-http.createServer(onRequest).listen(8000);
+module.exports = {
+  handleRequest: function(request, response) {
+      response.writeHead(200, {'Content-Type': 'text/html'});
 
+      var path = url.parse(request.url).pathname;
+      switch (path) {
+          case '/':
+              renderHTML('./index.html', response);
+              break;
+          case '/login':
+              renderHTML('./login.html', response);
+              break;
+          default:
+              response.writeHead(404);
+              response.write('Route not defined');
+              response.end();
+      }
+
+  }
+};
