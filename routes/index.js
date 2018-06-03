d82fb686e3c7dc551fc92fbb7b957407ae96bceb
Eric Perez <ibestudios@gmail.com>
2018-06-03 13:27:27 -0700

    set up routes in soutes/index.js

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,12 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* GET users listing. */
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/users/detail', function(req, res, next) {
+  res.send('detail');
+});
 
 module.exports = router;
