0191e213684dca5b2a647c04e96eb15a1af20f62
Eric Perez <ibestudios@gmail.com>
2018-06-03 12:43:32 -0700

    Changed server to render html response

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var http = require('http');
-var module1 = require('./module1');
-var module2 = require('./module2');
+var fs = require('fs');
 
 function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write(module2.myVariable);
-    module2.myFunction();
-    response.end();
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', null, function(error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
 }
 
 http.createServer(onRequest).listen(8000);
-
 
